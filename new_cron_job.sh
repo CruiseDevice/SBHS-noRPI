267cc92c27a991e836e565e0d560d096bb64a8ae
Akash Chavan <meakashchavan@gmail.com>
2017-11-16 09:52:38 +0530

    Added some more jobs to execute.

---
+++ new_cron_job.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Django 1.11 compatible
+
+# mdcNo = ('1','2','3','4','5','6','7','8','9','10','11','12','13','15','16')
+
+find $(pwd) -iname \*.pyc -exec rm -rfv {} \; #to delete all .pyc files
+
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+cd $DIR
+killall streamer
+rm production_static_files/img/webcam/*.jpeg
+source venv/bin/activate
+#source ./bin/activate
+python sbhs_server/switch_onn.py 
+python sbhs_server/scan_machines.py
+#python offline_reconnect.py
+python create_db.py
+python manage.py makemigrations
+python manage.py makemigrations tables
+python manage.py makemigrations account
+python manage.py makemigrations experiment
+python manage.py makemigrations slot
+python manage.py migrate
+python manage.py initialize_machines
+python manage.py generate_checksum
+touch index.wsgi
+python manage.py log_generator
+python check_future_slots.py
+
+#python log_generator.py
+date > date.txt
+sleep 2
+# python sbhs_server/switch_off.py 1
